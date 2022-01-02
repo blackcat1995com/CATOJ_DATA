fcb2c91408d8d6f791e2df25b139dbd00216be15
blackcatcoding <xypip@qq.com>
2022-01-02 18:12:50 +0800

    22.1.2

--- 1-1000/51-100/52-前缀统计/demo.cpp
+++ 1-1000/51-100/52-前缀统计/demo.cpp
@@ -2,59 +2,49 @@
 #include <cstdio>
 using namespace std;
 
-const int N = 1e6 +  10;
+const int N = 1e6 + 10;
+
+int n, m;
 int son[N][26], cnt[N], idx = 0;
 char str[N];
 
 void insert(const char* str){
 	int p = 0;
-	for(int i = 0; str[i]; i++){
-		int u = str[i] - 'a';
-		if(!son[p][u]) son[p][u] = ++idx;
-		p = son[p][u];
-	}
-	cnt[p]++;
+  	for(int i = 0; str[i]; i++){
+    	int u = str[i] - 'a';
+      	if(!son[p][u]) son[p][u] = ++idx;
+      	p = son[p][u];
+    }
+  	cnt[p]++;
 }
 
-int query(const char* str, int &ans){
+int query(const char* str){
 	int p = 0;
-	for(int i = 0; str[i]; i++){
-		int u = str[i] - 'a';
-		if(!son[p][u]) return 0;
-		p = son[p][u];
-		ans += cnt[p];
-	}
-	return cnt[p];
+  	int res = 0;
+  	for(int i = 0; str[i]; i++){
+    	int u = str[i] - 'a';
+      	if(!son[p][u]) return res;
+      	p = son[p][u];
+      	res += cnt[p];
+    }
+  
+  	return res;
 }
 
 int main() {
-
-	int n, m;
-	scanf("%d%d", &n, &m);
-
-	while(n--){
-		scanf("%s", str);
-		insert(str);
-	}
-
-	while(m--){
-		scanf("%s", str);
-		int ans = 0;
-		query(str, ans);
-		printf("%d\n", ans);
-	}
+  
+  	cin >> n >> m;
+  	while(n--){
+    	cin >> str;
+      	insert(str);
+    }
+  
+  	while(m--){
+    	cin >> str;
+      	cout << query(str) << endl;
+    }
 	
 	return 0;
 }
 
 
-/*
-4 3
-a
-ab
-bc
-abc
-abc
-efg
-bc
-*/
