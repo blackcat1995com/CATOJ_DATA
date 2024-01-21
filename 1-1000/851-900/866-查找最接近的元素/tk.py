2ad40b0bee690ae4e26a60b9e73cb5501ae2748e
blackcatcoding <xypip@qq.com>
2024-01-21 09:11:26 +0800

    2024.1.23

---
+++ 1-1000/851-900/866-查找最接近的元素/tk.py
@@ -0,0 +1,18 @@
+import os
+
+names = os.listdir("./data")
+
+# print(names)
+
+for i in names:
+    li = i.split('.')
+
+    # print(li)
+    # pre_name = 
+    if li[0][-2] == '1':
+    	new_name = "10" + '.' + li[1]
+    else:
+    	new_name = li[0][-1] + '.' + li[1]
+
+    os.rename("./data/" + i, "./data/" + new_name)
+    # print(new_name)
